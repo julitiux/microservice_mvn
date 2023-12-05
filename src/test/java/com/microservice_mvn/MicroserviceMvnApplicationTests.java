8f7fee79d5690262c5f7a7e96050238f5ba1b755
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-04 20:53:43 -0600

    starting new project microservice_mvn

---
+++ src/test/java/com/microservice_mvn/MicroserviceMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice_mvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
