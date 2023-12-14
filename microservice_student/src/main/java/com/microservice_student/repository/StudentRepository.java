36e22d5e8e6c5a9b40b117c48000dfdbbbd7ee7c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-13 22:47:51 -0600

    interface extends to CrudRepository; implement methods like findAllStudent and findAllByCourseId

--- microservice_student/src/main/java/com/microservice_student/repository/StudentRepository.java
+++ microservice_student/src/main/java/com/microservice_student/repository/StudentRepository.java
@@ -1,4 +1,18 @@
 package com.microservice_student.repository;
 
-public interface StudentRepository {
+import com.microservice_student.entities.Student;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+  @Query("SELECT s FROM Student s WHERE s.courseId = :courseId")
+  List<Student> findAllStudent(Long courseId);
+
+  List<Student> findAllByCourseId(Long courseId);
+
 }
