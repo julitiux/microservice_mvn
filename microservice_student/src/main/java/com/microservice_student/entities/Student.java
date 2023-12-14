a341eecea407ee28a7d63b7a72fc9e806196e36f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-13 18:28:50 -0600

    implement entity Student

---
+++ microservice_student/src/main/java/com/microservice_student/entities/Student.java
@@ -0,0 +1,25 @@
+package com.microservice_student.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@Table(name = "student")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String lastName;
+  private String email;
+  private Long courseId;
+
+}
