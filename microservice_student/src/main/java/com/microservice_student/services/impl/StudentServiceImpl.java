be98288ad4319068d085efb8b61ea9ab467531ed
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-15 23:35:52 -0600

    implement StudentServiceImp using the interface StudentService

--- microservice_student/src/main/java/com/microservice_student/services/impl/StudentServiceImpl.java
+++ microservice_student/src/main/java/com/microservice_student/services/impl/StudentServiceImpl.java
@@ -1,32 +1,35 @@
 package com.microservice_student.services.impl;
 
 import com.microservice_student.entities.Student;
+import com.microservice_student.repository.StudentRepository;
 import com.microservice_student.services.StudentService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+  private StudentRepository studentRepository;
+
   @Override
   public List<Student> findAll() {
-    return null;
+    return (List<Student>) studentRepository.findAll();
   }
 
   @Override
-  public Student findById(Long id) {
-    return null;
+  public Optional<Student> findById(Long id) {
+    return studentRepository.findById(id);
   }
 
   @Override
-  public Student save(Student sudent) {
-    return null;
+  public Student save(Student student) {
+    return studentRepository.save(student);
   }
 
   @Override
   public List<Student> findByIdCourse(Long courseId) {
-    return null;
+    return studentRepository.findAllByCourseId(courseId);
   }
-
 }
