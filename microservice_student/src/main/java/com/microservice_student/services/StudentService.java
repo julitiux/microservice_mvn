388b9e1aeaeb7fc72d367ed678011540c49dbd49
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-15 23:35:27 -0600

    modify the interface StudentService

--- microservice_student/src/main/java/com/microservice_student/services/StudentService.java
+++ microservice_student/src/main/java/com/microservice_student/services/StudentService.java
@@ -3,12 +3,13 @@ package com.microservice_student.services;
 import com.microservice_student.entities.Student;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentService {
 
   List<Student> findAll();
-  Student findById(Long id);
-  Student save(Student sudent);
+  Optional<Student> findById(Long id);
+  Student save(Student student);
   List<Student> findByIdCourse(Long courseId);
 
 }
