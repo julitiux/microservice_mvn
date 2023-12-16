4b403431785792a98380971d4d787698cc726edd
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-16 02:07:21 -0600

    implement methid saveStudent into StudentController

--- microservice_student/src/main/java/com/microservice_student/entities/StudentController.java
+++ microservice_student/src/main/java/com/microservice_student/entities/StudentController.java
@@ -1,4 +1,31 @@
 package com.microservice_student.entities;
 
+import com.microservice_student.services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/student")
 public class StudentController {
+
+  private final StudentService studentService;
+
+  public StudentController(StudentService studentService) {
+    this.studentService = studentService;
+  }
+
+  @PostMapping("/create")
+  public ResponseEntity<Object> saveStudent(@RequestBody Student student) {
+    var studentSaves = studentService.save(student);
+    return Objects.nonNull(studentSaves.getId())
+      ? new ResponseEntity<>(studentSaves, HttpStatus.CREATED)
+      : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  }
+
 }
