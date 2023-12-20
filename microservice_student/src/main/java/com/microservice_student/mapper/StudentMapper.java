ba0814f1cfb9f55e07f4856be6a4eeaad805bbbf
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-12-20 17:09:19 -0600

    implement method static of, with params Student and return a StudentDTO

--- microservice_student/src/main/java/com/microservice_student/mapper/StudentMapper.java
+++ microservice_student/src/main/java/com/microservice_student/mapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package com.microservice_student.mapper;
 
 import com.microservice_student.command.StudentCommand;
+import com.microservice_student.dto.StudentDTO;
 import com.microservice_student.entities.Student;
 
 public class StudentMapper {
@@ -11,6 +12,14 @@ public class StudentMapper {
       command.lastName(),
       command.email(),
       Long.parseLong(command.courseId())
+    );
+  }
+
+  public static StudentDTO of(Student student) {
+    return new StudentDTO(
+      student.getName(),
+      student.getLastName(),
+      student.getEmail()
     );
   }
 
