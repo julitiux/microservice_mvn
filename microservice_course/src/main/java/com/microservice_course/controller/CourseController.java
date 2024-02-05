f859c0a162ac91eca5e61913a6547348b985e828
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 01:02:55 -0600

    implement metjod findAll

--- microservice_course/src/main/java/com/microservice_course/controller/CourseController.java
+++ microservice_course/src/main/java/com/microservice_course/controller/CourseController.java
@@ -1,16 +1,16 @@
 package com.microservice_course.controller;
 
 import com.microservice_course.command.CourseCommand;
+import com.microservice_course.dto.CourseDto;
+import com.microservice_course.entities.Course;
 import com.microservice_course.mapper.CourseMapper;
 import com.microservice_course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -26,6 +26,16 @@ public class CourseController {
     return Objects.nonNull(courseSaved.getId()) ?
       new ResponseEntity<>(CourseMapper.of(courseSaved), HttpStatus.OK) :
       new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<List<CourseDto>> findAll(){
+    List<Course> courseList = courseService.findAll();
+    return !Objects.isNull(courseList) ?
+      new ResponseEntity<>(CourseMapper.listOf(courseList), HttpStatus.OK) :
+      new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+
   }
 
 }
