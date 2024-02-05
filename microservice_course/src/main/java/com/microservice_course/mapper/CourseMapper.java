1c94198b5862639ce033cf7538d31676806dd4b6
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 01:02:38 -0600

    implement static method of listOf, the arg is List<Course> and return List<CourseDto>

--- microservice_course/src/main/java/com/microservice_course/mapper/CourseMapper.java
+++ microservice_course/src/main/java/com/microservice_course/mapper/CourseMapper.java
@@ -4,6 +4,9 @@ import com.microservice_course.command.CourseCommand;
 import com.microservice_course.dto.CourseDto;
 import com.microservice_course.entities.Course;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CourseMapper {
 
   public static Course of(final CourseCommand courseCommand) {
@@ -12,6 +15,10 @@ public class CourseMapper {
 
   public static CourseDto of(final Course course) {
     return new CourseDto(course.getName(), course.getTeacher());
+  }
+
+  public static List<CourseDto> listOf(final List<Course> courseList){
+    return courseList.stream().map(CourseMapper::of).collect(Collectors.toList());
   }
 
 }
