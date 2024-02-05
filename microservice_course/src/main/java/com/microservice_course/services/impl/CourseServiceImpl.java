d374c4b84bb5ff0d9da9a1fb8439cabe8d44db99
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 00:23:15 -0600

    starting implement methods with bean CourseRepository

--- microservice_course/src/main/java/com/microservice_course/services/impl/CourseServiceImpl.java
+++ microservice_course/src/main/java/com/microservice_course/services/impl/CourseServiceImpl.java
@@ -1,7 +1,9 @@
 package com.microservice_course.services.impl;
 
 import com.microservice_course.entities.Course;
+import com.microservice_course.repository.CourseRepository;
 import com.microservice_course.services.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,18 +11,22 @@ import java.util.Optional;
 
 @Service
 public class CourseServiceImpl implements CourseService {
+
+  @Autowired
+  private CourseRepository courseRepository;
+
   @Override
   public List<Course> findAll() {
-    return null;
+    return (List<Course>) courseRepository.findAll();
   }
 
   @Override
   public Optional<Course> findById(Long id) {
-    return Optional.empty();
+    return courseRepository.findById(id);
   }
 
   @Override
   public Course save(Course course) {
-    return null;
+    return courseRepository.save(course);
   }
 }
