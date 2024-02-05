e428e0668990609eec5dd387240c50841a28c39f
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 01:30:36 -0600

    add a generic List with a method findAllStudentByCourse

--- microservice_course/src/main/java/com/microservice_course/client/StudentClient.java
+++ microservice_course/src/main/java/com/microservice_course/client/StudentClient.java
@@ -2,6 +2,11 @@ package com.microservice_course.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
+import java.util.List;
+
 @FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
 public interface StudentClient {
+
+  List<?> findAllStudentByCourse();
+
 }
