11185723d42d5a35cdbb0083d3d4346a345b24a7
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 00:34:15 -0600

    implement method static of into CouseMapper

--- microservice_course/src/main/java/com/microservice_course/mapper/CourseMapper.java
+++ microservice_course/src/main/java/com/microservice_course/mapper/CourseMapper.java
@@ -1,4 +1,12 @@
 package com.microservice_course.mapper;
 
+import com.microservice_course.command.CourseCommand;
+import com.microservice_course.entities.Course;
+
 public class CourseMapper {
+
+  public static Course of(final CourseCommand courseCommand){
+    return new Course(null, courseCommand.name(), courseCommand.teacher());
+  }
+
 }
