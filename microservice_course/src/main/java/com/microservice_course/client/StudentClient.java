7b6d48fee2516f4f4ed5978b64ac7e4d6ef9776a
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 01:31:23 -0600

    add the same mapping of StudentController

--- microservice_course/src/main/java/com/microservice_course/client/StudentClient.java
+++ microservice_course/src/main/java/com/microservice_course/client/StudentClient.java
@@ -1,12 +1,14 @@
 package com.microservice_course.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
 public interface StudentClient {
 
+  @GetMapping("/search_by_course/{idCourse}")
   List<?> findAllStudentByCourse();
 
 }
