56b5ce40de232c30a0b07cc200855c8a0e05389e
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-02-05 01:29:24 -0600

    add url with localhost

--- microservice_course/src/main/java/com/microservice_course/client/StudentClient.java
+++ microservice_course/src/main/java/com/microservice_course/client/StudentClient.java
@@ -2,6 +2,6 @@ package com.microservice_course.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "msvc-student")
+@FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
 public interface StudentClient {
 }
